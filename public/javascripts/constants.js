b4d92d270ec4451854c9554df6a0b4afb941e0ae
unknown <ign@Z10CARMAD005.ign.fomento.es>
2019-07-27 18:23:06 +0200

    version 2.0.0

--- public/javascripts/constants.js
+++ public/javascripts/constants.js
@@ -1,4 +1,4 @@
 //Root to the app URL
-// const ROOT = 'http://localhost:3000';
-const ROOT = 'https://avillena-pfg.herokuapp.com';
+const ROOT = 'http://localhost:3000';
+//const ROOT = 'https://avillena-pfg.herokuapp.com';
 
