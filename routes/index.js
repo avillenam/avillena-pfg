f775891ab33ae6c3b16b71bec9cd1f43aba2ed9e
avillenam <a_villenam@hotmail.com>
2019-11-18 15:07:12 +0100

    version 2.8.5

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 const db = require('../queries');
 
 // var pg = require("pg");position-vehicle
-// var conString = "postgres://postgres:postgres@localhost:5432/api";
+//var conString = "postgres://postgres:postgres@localhost:5432/api";
 // var conString = "postgres://wzkowhhekyvcbh:dbc37ca58c23fa2edf7ed4af8319e00316de9aaf1defbb8cac1fd86500704f6a@ec2-107-20-173-2.compute-1.amazonaws.com:5432/d2346t6en0926l";
 
 // const {Pool} = require('pg');
