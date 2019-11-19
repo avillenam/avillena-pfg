95e1b4f2b6f9eb7a9bba44697a9c80a5b9035502
avillenam <a_villenam@hotmail.com>
2019-11-19 07:52:52 +0100

    version 2.9

--- queries.js
+++ queries.js
@@ -1,8 +1,8 @@
 //var Pool = require("pg").Pool;
 var pg = require("pg");
 
-var conString = "postgres://postgres:postgres@localhost:5432/api";
-//var conString = "postgres://wzkowhhekyvcbh:dbc37ca58c23fa2edf7ed4af8319e00316de9aaf1defbb8cac1fd86500704f6a@ec2-107-20-173-2.compute-1.amazonaws.com:5432/d2346t6en0926l";
+// var conString = "postgres://postgres:postgres@localhost:5432/api";
+var conString = "postgres://wzkowhhekyvcbh:dbc37ca58c23fa2edf7ed4af8319e00316de9aaf1defbb8cac1fd86500704f6a@ec2-107-20-173-2.compute-1.amazonaws.com:5432/d2346t6en0926l";
 
 //Fechas por defecto
 var ahora = new Date();
