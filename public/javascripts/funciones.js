23685a6d8ef652c21a7999b39956ab2caedc9a94
avillenam <a_villenam@hotmail.com>
2020-01-18 20:16:53 +0100

    version 5.2

--- public/javascripts/funciones.js
+++ public/javascripts/funciones.js
@@ -191,23 +191,32 @@ function getDrivers() {
         // Crea un elemento por cada conductor
         myItems.push("" +
             "<div class='resultItem'>" +
-            "<img class='list-thumbnail' src='/images/" + gender_icon + "' width='50'>" +
-            "<div href='#" + id + "' class='details'>" +
-            "<div class='list-group-item-heading'><i class='fa fa-hashtag'></i>" + id + "</div>" +
-            "<div title='Email'><i class='fa fa-at'></i>" + email + "</div>" +
-            "<div title='Nombre de conductor'><i class='glyphicon glyphicon-user'></i>" + name + " " + surname + "</div>" +
-            "<div title='Fecha de nacimiento'><i class='glyphicon glyphicon-calendar'></i>" + birthdate + "</div>" +
-            "<div title='Género'><i class='fa fa-genderless'></i>" + genre + "</div>" +
-            "<div title='Número de móvil'><i class='fa fa-mobile-alt'></i>" + mobile_number + "</div>" +
-            "<div title='Vehículo'><i class='fa'>&#x" + vehicle_mini_icon + "</i>" + id_vehicle + "</div>" +
-            "</div>" +
+                "<img class='list-thumbnail' src='/images/" + gender_icon + "' width='50'>" +
+                "<div href='#" + id + "' class='details'>" +
+                    "<div class='list-group-item-heading'><i class='fa fa-hashtag'></i>" + id + "</div>" +
+                    "<div title='Email'><i class='fa fa-at'></i>" + email + "</div>" +
+                    "<div title='Nombre de conductor'><i class='glyphicon glyphicon-user'></i>" + name + " " + surname + "</div>" +
+                    "<div title='Fecha de nacimiento'><i class='glyphicon glyphicon-calendar'></i>" + birthdate + "</div>" +
+                    "<div title='Género'><i class='fa fa-genderless'></i>" + genre + "</div>" +
+                    "<div title='Número de móvil'><i class='fa fa-mobile-alt'></i>" + mobile_number + "</div>" +
+                    "<div title='Vehículo'><i class='fa'>&#x" + vehicle_mini_icon + "</i>" + id_vehicle + "</div>" +
+                "</div>" +
+                "<div class='tileActions'>" +
+                    "<a title='Eliminar conductor' class='deleteIcon delete-driver' data-id=" + id + "><i class='fa fa-trash'></i></a>" +
+                    "<a title='Editar conductor' class='editIcon edit-driver' data-toggle='modal' data-target='#editFormDriver' " +
+                    " data-id=" + id + " data-name=" + name + " data-surname=" + surname + " data-birthdate= " + birthdate +
+                    " data-genre=" + genre + " data-mobile_number= " + mobile_number + " data-email=" + email +
+                    " data-available=" + available +
+                    ">" +
+                    "<i class='fa fa-edit'></i>" +
+                    "</a>" +
+                "</div>" +
             "</div>");
-
     }
     $('#drivers_results').html(myItems.join(''));
     //console.log('Conductores capturados: ')
     //console.log(driversJSON);
-    //updateFunctions();
+    updateFunctions();
 }
 
 // Obtiene un objeto JSON a partir de su id
@@ -580,37 +589,45 @@ function createVehicleHTMLinfo() {
 
     // Crea elementos HTML para cada vehículo
     myItems.push("" +
-        "<h3 class='mb-1 font-weight-semi-bold' title='Matrícula vehículo'>" + matricula + "</h3>" +
-        "<h5 title='Última dirección registrada del vehículo'>" + address + "</h5>" +
-        "<h5 title='Ultimas coordenadas registradas del vehículo'>Lon/Lat: " + coordenadas + "</h5>" +
-        "<hr style='color: #566167;'/>" +
-        "<div title='Vehículo'><h4 style='font-weight: bold'>Datos del Vehículo</h4>" +
-        "<a title='Editar Vehiculo' class='editIcon edit-vehicle' data-toggle='modal' data-target='#editFormVehicle' " +
-        " data-id=" + parseInt(id) +
-        " data-type=" + type +
-        " data-brand=" + marca +
-        " data-model=" + modelo +
-        " data-passengers=" + pasajeros +
-        " data-fuel=" + fuel +
-        " data-available=" + available +
-        ">" +
-        "<i class='fa fa-edit'></i>" +
-        "</a>" +
-        "<a title='Eliminar vehiculo' class='deleteIcon delete-vehicle' data-id=" + id + "><i class='fa fa-trash'></i></a>" +
-        "<h4><i class='fa'>&#x" + vehicle_mini_icon + "</i> [" + id + "]: " + marca + ", " + modelo + "</h4>" +
-        "<p>Pasageros: " + pasajeros + "</p>" +
-        "<p>Combustible: " + fuel + "</p>" +
+        "<div>" +
+            "<h3 class='mb-1 font-weight-semi-bold' title='Matrícula vehículo'>" + matricula + "</h3>" +
+            "<h5 title='Última dirección registrada del vehículo'>" + address + "</h5>" +
+            "<h5 title='Ultimas coordenadas registradas del vehículo'>Lon/Lat: " + coordenadas + "</h5>" +
         "</div>" +
-        "<div class='align-middle' title='Última veolicidad registrada Velocidad'>Velocidad: " + velocidad + " km/h</div><hr style='color: #566167;'/>" +
-        "<div><h4 style='font-weight: bold'>Conductor Asignado:</h4><p class='mb-0' title='Conductor'>" + conductor_asignado + "</p></div>"+
-        "<a title='Crear relación Vehículo-Conductor' class='relationIcon create-relation' data-toggle='modal' data-target='#formVehicleDriver' " +
-        ">" +
-        "<i class='fa fa-plus-square'></i>" +
-        "</a>" +
-        "<a title='Editar relación Vehículo-Conductor' class='editRelationIcon edit-relation' data-toggle='modal' data-target='#editRelationVehicleDriver'>" +
-        "<i class='fa fa-edit'></i>" +
-        "</a>" +
-        "<a title='Eliminar relación Vehículo-Conductor' class='deleteRelationIcon delete-relation' data-id=" + id + "><i class='fa fa-trash'></i></a>"
+        "<hr style='color: #566167;'/>" +
+        "<div>" +
+            "<h4 title='Vehículo' style='font-weight: bold'>Datos del Vehículo</h4>" +
+            "<div class='tileActions'>" +
+                "<a title='Editar Vehiculo' class='editIcon edit-vehicle' data-toggle='modal' data-target='#editFormVehicle' " +
+                " data-id=" + parseInt(id) +
+                " data-type=" + type +
+                " data-brand=" + marca +
+                " data-model=" + modelo +
+                " data-passengers=" + pasajeros +
+                " data-fuel=" + fuel +
+                " data-available=" + available +
+                ">" +
+                "<i class='fa fa-edit'></i>" +
+                "</a>" +
+                "<a title='Eliminar vehiculo' class='deleteIcon delete-vehicle' data-id=" + id + "><i class='fa fa-trash'></i></a>" +
+            "</div>" +
+            "<h4><i class='fa'>&#x" + vehicle_mini_icon + "</i> [" + id + "]: " + marca + ", " + modelo + "</h4>" +
+            "<p title='Pasajeros' Pasageros: " + pasajeros + "</p>" +
+            "<p title='Tipo de combustible'>Combustible: " + fuel + "</p>" +
+            "<p class='align-middle' title='Última veolicidad registrada Velocidad'>Velocidad: " + velocidad + " km/h</p>" +
+        "</div>" +
+        "<hr style='color: #566167;'/>" +
+        "<div>" +
+            "<h4 style='font-weight: bold'>Conductor Asignado:</h4>" +
+            "<div class='tileActions'>" +
+                "<a title='Crear relación Vehículo-Conductor' class='relationIcon create-relation' data-toggle='modal' data-target='#formVehicleDriver'><i class='fa fa-plus-square'></i></a>" +
+                "<a title='Editar relación Vehículo-Conductor' class='editRelationIcon edit-relation' data-toggle='modal' data-target='#editRelationVehicleDriver'><i class='fa fa-edit'></i></a>" +
+                //TODO: una vez creada o editada la relación vehículo-conductor, actualizar el panel con los nuevos datos a través de jQuery
+                "<a title='Eliminar relación Vehículo-Conductor' class='deleteIcon delete-relation' data-id=" + id + "><i class='fa fa-trash'></i></a>"+
+            "</div>" +
+            "<p class='mb-0' title='Conductor'>" + conductor_asignado + "</p>" +
+        "</div>"
+
     );
 
     // Añade los elementos al div #info_result
@@ -785,6 +802,7 @@ function muestraColaVehiculo(id) {
 
     });
 
+    updateFunctions();
 }
 
 
