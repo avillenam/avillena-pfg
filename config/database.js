50ac27ef103adc0360fa6b016ac3381ee066aac9
avillenam <villenageodesia@gmail.com>
2022-06-30 08:47:02 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
     // 'url': 'postgres://postgres:postgres@localhost:5432/api'
-    'url': 'postgres://wzkowhhekyvcbh:dbc37ca58c23fa2edf7ed4af8319e00316de9aaf1defbb8cac1fd86500704f6a@ec2-107-20-173-2.compute-1.amazonaws.com:5432/d2346t6en0926l'
+    //'url': 'postgres://wzkowhhekyvcbh:dbc37ca58c23fa2edf7ed4af8319e00316de9aaf1defbb8cac1fd86500704f6a@ec2-107-20-173-2.compute-1.amazonaws.com:5432/d2346t6en0926l'
+    'url': 'postgres://gltbttzjsnhslx:0e5170a75dc8961515c3b5d2af7e874c7981d35cf580ae46eb1c38d97833d426@ec2-54-159-126-187.compute-1.amazonaws.com:5432/df11rr5nod4181'
 }
