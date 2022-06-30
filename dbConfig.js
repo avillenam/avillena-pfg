a887fdc067329316b5b58b451ca7de8b20023ba4
avillenam <villenageodesia@gmail.com>
2022-06-30 08:53:16 +0200

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -3,7 +3,7 @@
 const { Pool } = require("pg");
 
 // const connectionString = 'postgres://postgres:postgres@localhost:5432/api';
-const connectionString = 'postgres://wzkowhhekyvcbh:dbc37ca58c23fa2edf7ed4af8319e00316de9aaf1defbb8cac1fd86500704f6a@ec2-107-20-173-2.compute-1.amazonaws.com:5432/d2346t6en0926l';
+const connectionString = 'postgres://gltbttzjsnhslx:0e5170a75dc8961515c3b5d2af7e874c7981d35cf580ae46eb1c38d97833d426@ec2-54-159-126-187.compute-1.amazonaws.com:5432/df11rr5nod4181';
 
 
 const pool = new Pool({
